dbeec86ecf37823e0cdf0875a3f7e58c336b2373
TalLicht <tallicht89@gmail.com>
2017-12-23 15:14:01 +0200

    changes theme to lion king, added gifs and music

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var aud = document.getElementById("myAudio");
+var aud2 = document.getElementById("myAudio");
 var numOfCards = 12;
 var clickCounter = 0, wrongGuessesCount = 0, pairsCounter = [];
 var firstCardId = 0, secondCardId = 0, firstCard="", secondCard = "";
@@ -5,51 +7,40 @@ var gameBoard = document.getElementById("gameBoard");
 var ableToFlip = true;
 var pairsArr = ["firstPair","firstPair","secondPair","secondPair","thirdPair","thirdPair","forthPair","forthPair","fifthPair","fifthPair","sixthPair","sixthPair"];
 //window.addEventListener("load",level);
-document.getElementById("newGame").addEventListener("click",level);
-document.getElementById("newGameModal").addEventListener("click",level);
+document.getElementById("newGame").addEventListener("click",createBoard);
+document.getElementById("newGameModal").addEventListener("click",createBoard);
 
 function Game(){
 	
 };
 
-function level(e){
-	document.getElementById("levelsModal").style.display = "block";
-	document.getElementById("easy").addEventListener("click",function(){
-       numOfCards = 12;
-       createBoard();
-	});
-	document.getElementById("medium").addEventListener("click",function(){
-       numOfCards = 30;
-       createBoard();
-       document.getElementById("gameBoard").style.margin = "auto 100px";
-	});
-	document.getElementById("hard").addEventListener("click",function(){
-       numOfCards = 36;
-       createBoard();
-       document.getElementById("gameBoard").style.margin = "auto";
-	});
-}
 
-function pickBackSide(){
+// function pickBackSide(){
 
-}
+// }
 
-function picktheme(){
+// function picktheme(){
 
-}
+// }
 
 //Game.prototype.createBoard = function(){
 function createBoard(){
-	document.getElementById("levelsModal").style.display = "none";
+	aud.pause();
+    aud.innerHTML = "<source src='I Just Cant Wait To Be King.mp3' type='audio/mp3'>";
+    aud.load();
+    aud.play();
 	document.getElementById("theModal").style.display = "none";
 	gameBoard.innerHTML = "";
 	wrongGuessesCount = 0;
-	for (var i = 1 ; i <= numOfCards ; i++){
-		var imgHolder = document.createElement('div');
-		gameBoard.appendChild(imgHolder);
-		imgHolder.setAttribute("id", [i]);
-		imgHolder.setAttribute("class", "card cardFaceDown");
-		imgHolder.addEventListener("click",flipCards);
+	document.getElementById("wrongGuesses").innerHTML = "wrong guesses: " + wrongGuessesCount;
+	for (var i = 0 ; i < 3 ; i++){
+		for (var j = 0 ; j < 4 ; j++){
+			var imgHolder = document.createElement('div');
+			gameBoard.appendChild(imgHolder);
+			imgHolder.setAttribute("id", [i+3*j]);
+			imgHolder.setAttribute("class", "card cardFaceDown");
+			imgHolder.addEventListener("click",flipCards);
+		}
 	}
 	shuffle(pairsArr);
 	pairsCounter = [];
@@ -104,6 +95,10 @@ function flipCards(e){
       	localStorage.setItem('numOfWrongGuesses',wrongGuessesCount);
       }
       if(pairsCounter.length == (numOfCards/2)){
+      	aud.pause();
+      	aud.innerHTML = "<source src='cheers.wav' type='audio/wav'>";
+      	aud.load();
+      	aud.play();
       	document.getElementById("theModal").style.display = "block";
       	var wrongGuesses = localStorage.getItem('numOfWrongGuesses');
       	document.getElementById("modalBodyText").innerHTML = "You had " + wrongGuesses + " wrong gusses";
